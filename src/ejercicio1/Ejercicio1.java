96a34e99de4a7ee2fb17e49636ea76674f9eac96
Estudiante <Estudiante@10.203.1.255>
2016-08-09 15:18:53 -0500

    Clase 3, ejercicios de practica

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * @author Estudiante
+ */
+
+
+    /**
+     * @param args the command line arguments
+     */
+public class Ejercicio1 {    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 3;
+        
+        
+        while (x > 0){
+        if (x > 2){
+            System.out.print ("a");
+        }
+        
+        if (x == 2){
+            System.out.print("b c");
+        }
+        
+        if (x == 1){
+            System.out.print("d");
+            x = x + 1;
+        }
+        
+        
+            x = x-1;
+            System.out.print("-");
+        } 
+    }
+}
+
